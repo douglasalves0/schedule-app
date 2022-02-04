374dec2ea00bbb2aef908fb4fe1b0625abce5ac1
Douglas <douglas.sousa@ccc.ufcg.edu.br>
2022-02-04 08:42:37 -0300

    created schedule migration and renamed session migration

--- migration/1643930988092-Session.ts
+++ migration/1643930988092-Session.ts
@@ -1,6 +1,6 @@
 import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
-export class PostRefactoring1643848950714 implements MigrationInterface {
+export class Session1643848950714 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
--- migration/1643934396736-Schedule.ts
+++ migration/1643934396736-Schedule.ts
@@ -1,11 +1,54 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import {MigrationInterface, QueryRunner, Table, TableForeignKey} from "typeorm";
 
 export class Schedule1643934396736 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
+        await queryRunner.createTable(
+            new Table({
+                name: 'schedule',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'uuid',
+                        isPrimary: true,
+                        generationStrategy: 'uuid',
+                        default: 'uuid_generate_v4()',
+                    },{
+                        name: 'session_id',
+                        type: 'uuid',
+                    },{
+                        name: 'code',
+                        type: 'varchar'
+                    },{
+                        name: 'type',
+                        type: 'varchar'
+                    },{
+                        name: 'status',
+                        type: 'varchar'
+                    },{
+                        name: 'date',
+                        type: 'timestamp',
+                        default: 'now()'
+                    },{
+                        name: 'error',
+                        type: 'varchar'
+                    }
+                ]
+            })
+        );
+        await queryRunner.createForeignKey(
+            'schedule',
+            new TableForeignKey({
+                columnNames: ['session_id'],
+                referencedTableName: 'session',
+                referencedColumnNames: ['id']
+            })
+        );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("schedule");
     }
 
 }
