72cf4cd2413b50ce0f39f39b2a752edc88ed818a
Douglas <douglas.sousa@ccc.ufcg.edu.br>
2022-02-03 21:26:58 -0300

    added SessionMessage migration

---
+++ migration/1643932742330-SessionMessage.ts
@@ -0,0 +1,51 @@
+import {MigrationInterface, QueryRunner, Table, TableForeignKey} from "typeorm";
+
+export class SessionMessage1643932742330 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
+        await queryRunner.createTable(
+            new Table({
+                name: 'session_message',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'uuid',
+                        isPrimary: true,
+                        generationStrategy: 'uuid',
+                        default: 'uuid_generate_v4()',
+                    },{
+                        name: 'session_id',
+                        type: 'uuid',
+                    },{
+                        name: 'from',
+                        type: 'varchar'
+                    },{
+                        name: 'to',
+                        type: 'varchar'
+                    },{
+                        name: 'message',
+                        type: 'varchar'
+                    },{
+                        name: 'date',
+                        type: 'timestamp',
+                        default: 'now()'
+                    }
+                ]
+            })
+        );
+        await queryRunner.createForeignKey(
+            'session_message',
+            new TableForeignKey({
+                columnNames: ['session_id'],
+                referencedTableName: 'session',
+                referencedColumnNames: ['id']
+            })
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("session_message");
+    }
+
+}
