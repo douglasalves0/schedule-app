f930ac787beb2d85bf83c6ef6a2b0fb85ce378d0
Douglas <douglas.sousa@ccc.ufcg.edu.br>
2022-02-07 10:15:58 -0300

    finished checkDate

--- src/services/handlers/handle.create.notification.ts
+++ src/services/handlers/handle.create.notification.ts
@@ -4,7 +4,6 @@ import { v4 as uuidv4 } from 'uuid';
 import { SessionRepository } from 'src/repositories/session.repository';
 import { SessionMessageRepository } from 'src/repositories/session.message.repository';
 import { ValidDateNeeded } from 'src/utils/constants';
-import { TypeOrmOptionsFactory } from '@nestjs/typeorm';
 
 export class CreateNotificationHandler implements Message{
     public handle(message: MessageDto, sessionId: uuidv4) {
@@ -25,11 +24,7 @@ export class CreateNotificationHandler implements Message{
             to: botNumber
         });
 
-        var isValidDate  = !isNaN(Date.parse(givenDate));
-
-        console.log(this.checkDate(givenDate));
-
-        if(!isValidDate){
+        if(!this.checkDate(givenDate)){
             sessionMessageRepo.save({
                 date: new Date(),
                 direction: "out",
@@ -42,69 +37,87 @@ export class CreateNotificationHandler implements Message{
             return;
         }
 
-        console.log("A data é válida\n");
+        var now = new Date();
+        var userDate = new Date(givenDate);
+
+        if(now >= userDate){
+            sessionMessageRepo.save({
+                date: new Date(),
+                direction: "out",
+                from: botNumber,
+                message: ValidDateNeeded,
+                session_id: sessionId,
+                to: userNumber
+            });
+            console.log("Mensagem do bot:\n" + ValidDateNeeded);
+            return;
+        }
+
         //mecher no schedule
 
     }
     public checkDate(date: string): boolean{
         var twoDots:number[] = [];
         var bars:number[] = [];
-        console.log("oi");
         for(var i=0;i<date.length;i++){
             if(date[i] == '/'){
                 bars.push(i);
+                continue;
             }
             if(date[i] == ':'){
                 twoDots.push(i);
+                continue;
             }
-            if(!(date[i].toLowerCase() != date[i].toUpperCase())){
+            if(!(date[i] >= '0' && date[i] <= '9') && date[i] != ' '){
                 return false;
             }
         }
         if(twoDots.length != 1 || bars.length != 2){
             return false;
         }
-        if(twoDots[0] > bars[1]){
+        if(twoDots[0] < bars[1]){
             return false;
         }
-        var strNumber1: string;
-        for(var i=0;i<=twoDots[0]-1;i++){
+        var strNumber1 = "";
+        for(var i=0;i<=bars[0]-1;i++){
             strNumber1 += date[i];
             if(date[i]==' '){
                 return false;
             }
         }
-        var strNumber2: string;
-        for(var i=twoDots[0]+1;i<=twoDots[1]-1;i++){
+        var strNumber2 = "";
+        for(var i=bars[0]+1;i<=bars[1]-1;i++){
             strNumber2 += date[i];
             if(date[i]==' '){
                 return false;
             }
         }
-        var strNumber3: string;
+        var strNumber3 = "";
         var p = bars[0]-1;
-        for(var i=twoDots[1]+1;i<=bars[0]-1;i++){
+        for(var i=bars[1]+1;i<=twoDots[0]-1;i++){
             if(date[i]==' '){
                 p = i;
                 break;
             }
             strNumber3 += date[i];
         }
-        var strNumber4: string;
-        for(var i=p+1;i<=bars[0]-1;i++){
+        var strNumber4 = "";
+        for(var i=p+1;i<=twoDots[0]-1;i++){
             strNumber4 += date[i];
             if(date[i]==' '){
                 return false;
             }
         }
-        var strNumber5: string;
-        for(var i=bars[0]+1;i<=date.length-1;i++){
+        var strNumber5 = "";
+        for(var i=twoDots[0]+1;i<=date.length-1;i++){
             strNumber5 += date[i];
             if(date[i]==' '){
                 return false;
             }
         }
-        console.log(strNumber1 + "/" + strNumber2 + "/" + strNumber3 + " " + strNumber4 + ":" + strNumber5);
+        if(isNaN(Date.parse(date))){
+            return false;
+        }
         return true;
     }
 }
